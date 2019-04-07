47d424c9ab863bd050976f97b8a7025dde29d3ea
Aaron Lenhart <aaron.lenhart1@gmail.com>
2019-04-07 13:58:56 -0400

    Was using older version of python this works on 3+

--- Number Guessing Game /guessing_game.py
+++ Number Guessing Game /guessing_game.py
@@ -34,7 +34,7 @@ def play_agin():
   
   while True :
     try:
-      restart_game = str(raw_input("Current low score is {} See if you can beat that would you like you try agin [Y]es/[N]o: ".format(best_score)))  
+      restart_game = str(input("Current low score is {} See if you can beat that would you like you try agin [Y]es/[N]o: ".format(best_score)))  
       if restart_game.upper() == "Y" or restart_game.upper() == "YES":
           start_game()
       elif restart_game.upper() == "N" or restart_game.upper() == "NO":
